1a2b3ca898f0457992e6e6223f97d6b86da956c3
BEDRUDIN ABDIHALIM MOHAMOUD <bmahamued@gmail.com>
2025-07-23 19:37:39 +0800

    yaslm

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <div>
       <h1 className="text-3xl font-bold underline text-center bg-red-500 text-white p-4">
         waankusalmaaye! </h1>
+        <h1>what did you muhydin </h1>
     </div>
   )
 }
