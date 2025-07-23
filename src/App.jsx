a8373922cd6e0585638722085796bf923b8366a0
Muhyadiin M. Jama <muja@outlook.my>
2025-07-23 19:03:21 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center bg-red-500 text-white p-4">
+        Hello world! </h1>
+    </div>
+  )
+}
+
+export default App
